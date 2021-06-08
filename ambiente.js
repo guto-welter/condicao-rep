9a315702a6f8746c95b8c7e8f544ff5b11bd69fb
Gustavo Welter <69959822+guto-welter@users.noreply.github.com>
2021-06-08 16:33:18 -0300

    Add files via upload

---
+++ ambiente.js
@@ -0,0 +1,18 @@
+    
+    
+    
+    
+    // CONDIÇÃO DE REPETIÇÃO COM TESTE NO FINAL
+    var c = 1
+    do {
+        console.log(`Passo ${c}`)
+        c++
+    } while (c <= 6)
+
+    // CONDIÇÃO DE REPETIÇÃO COM TESTE NO INICIO
+    /* var c = 1
+    while (c <= 6) {
+        console.log(`Passo ${c}`)
+        c++
+    }
+    */ 
